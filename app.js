bc54272eb9d330a3fe7aa557c80a82fad88fa63b
Brita Budnick <bhh2@pdx.edu>
2021-05-25 20:01:17 -0700

    Added dummy form to the repgame route

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var logger = require('morgan');
 var cors = require('cors');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var testRouter = require('./routes/test');
 var gameRouter = require('./routes/repgame');
 var linkDirRouter = require('./routes/linkdir');
@@ -26,7 +25,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/test', testRouter);
 app.use('/repgame', gameRouter);
 app.use('/linkdir', linkDirRouter);
