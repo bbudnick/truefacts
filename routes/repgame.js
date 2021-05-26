6ba63aca5627c9381a371ac9cb31d1f81cdaf303
Brita Budnick <bhh2@pdx.edu>
2021-05-26 09:55:18 -0700

    Added image to Research Game page

--- routes/repgame.js
+++ routes/repgame.js
@@ -1,8 +1,11 @@
 var express = require("express");
 var router = express.Router();
+const app = new express(); 
 
 router.get("/", function (req, res, next) {
     res.render('repgame');
 });
+
+app.use(express.static(__dirname+'/public'));
 
 module.exports = router;
