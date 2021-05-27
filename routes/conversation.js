5af623e49d9266e3d9358c3f6043529d61313119
Brita Budnick <bhh2@pdx.edu>
2021-05-26 18:15:59 -0700

    Experimenting with layout and added csv of google covid data

--- routes/conversation.js
+++ routes/conversation.js
@@ -1,8 +1,8 @@
 var express = require("express");
 var router = express.Router();
 
-router.get("/", function (req, res, next) {
-    res.send("How to have a productive conversation about difficult things");
+router.get('/', function (req, res, next) {
+    res.render('conversation');
 });
 
 module.exports = router;
