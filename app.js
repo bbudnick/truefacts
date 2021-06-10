e0cfbfa695fc9f6fdb9797363bbdd3236575c5f7
Brita Budnick <bhh2@pdx.edu>
2021-06-09 17:06:21 -0700

    Looking more presentable

--- app.js
+++ app.js
@@ -37,6 +37,7 @@ app.use('/repgameplay', repGamePlay);
 app.use('/papernope', paperNope);
 app.use('/about', about);
 
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
