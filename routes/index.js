216f19373031eb88e842fb40792c6d8c5da94c3c
Brita Budnick <bhh2@pdx.edu>
2021-06-10 12:31:47 -0700

    Successfully grabbed USA data from covid data project query in index.js

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,34 @@
-/* 
-  TODO: 
-
-  -- Welcome and explanation 
-  -- Table of keywords on COVID that have definitions and links to the article
-      where the keyword was used 
-  -- Dashboard homepage/hamburger menu 
-
-*/
-
-
-
 var express = require('express');
 var router = express.Router();
+var unirest = require("unirest");
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'True Facts' });
+
+  var req = unirest("GET", "https://covid-19-data.p.rapidapi.com/report/country/name");
+
+  req.query({
+    "name": "USA",
+    "date": "2020-06-10"
+  });
+
+  req.headers({
+    "x-rapidapi-key": "d93b61c9demshbf08c34f5be30dbp1e14c0jsn1124be3651ef",
+    "x-rapidapi-host": "covid-19-data.p.rapidapi.com",
+    "useQueryString": true
+  });
+
+
+  req.end(function (res) {
+    if (res.error) throw new Error(res.error);
+
+    console.log(res.body);
+  });
 });
 
 module.exports = router;
+
+
+
