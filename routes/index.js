09e9577dd958ab6c208e7d6636230a5eb9367bf3
Brita Budnick <bhh2@pdx.edu>
2021-06-10 13:45:11 -0700

    Reworked order of covid data API call because headers

--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,36 @@
 var express = require('express');
 var router = express.Router();
 var unirest = require("unirest");
+const jade = require('jade');
+const bodyParser = require("body-parser");
 
+router.use(
+  bodyParser.urlencoded({
+    extended: false,
+    limit: 1024,
+  })
+);
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'True Facts' });
-
-  var req = unirest("GET", "https://covid-19-data.p.rapidapi.com/report/country/name");
-
-  req.query({
-    "name": "USA",
-    "date": "2020-06-10"
-  });
-
-  req.headers({
-    "x-rapidapi-key": "d93b61c9demshbf08c34f5be30dbp1e14c0jsn1124be3651ef",
-    "x-rapidapi-host": "covid-19-data.p.rapidapi.com",
-    "useQueryString": true
-  });
-
-
-  req.end(function (res) {
-    if (res.error) throw new Error(res.error);
-
-    console.log(res.body);
-  });
+  let getCurData = async () => {
+    const url = unirest("GET", "https://covid-19-data.p.rapidapi.com/country");
+    url.query({
+      "name": "usa",
+    });
+    url.headers({
+      "x-rapidapi-key": "d93b61c9demshbf08c34f5be30dbp1e14c0jsn1124be3651ef",
+      "x-rapidapi-host": "covid-19-data.p.rapidapi.com",
+      "useQueryString": true
+    });
+    url.end(function (res) {
+      if (res.error) throw new Error(res.error);
+      jade.compile('index')
+      console.log(res.body);
+    });
+  }
+  getCurData();
+  res.render('index', { title: 'True Facts', curData: res.body });
 });
 
 module.exports = router;
-
-
-
